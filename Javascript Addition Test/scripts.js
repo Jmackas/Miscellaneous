b4660d0c60537ee44ab45493786f6106bb30eca8
Jmackas <jamesrossmackay@gmail.com>
2017-09-24 10:05:00 +0800

    Test failed rip

--- Javascript Addition Test/scripts.js
+++ Javascript Addition Test/scripts.js
@@ -1,8 +1,13 @@
 
+
+
 // parsing of variable one
 var numOne = document.getElementById('numOneInput');
 
 // parsing of variable two
 var numTwo = document.getElementById('numTwoInput');
 
+// calculations
+var finalNum = numTwo * numOne;
 
+document.getElementById("finalNumOutput").innerHTML = finalNum;
